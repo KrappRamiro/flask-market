a80cc9733b7aec04970c215f367ba8ce58cd253a
Ramiro Krapp <krappramiro@disroot.org>
2022-06-10 11:39:59 -0300

    added a login system

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -8,4 +9,7 @@ app.config['SECRET_KEY'] = '35c4ec6ef6b94d31f828f785'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager(app)
+
 from market import routes
+
--- market/models.py
+++ market/models.py
@@ -1,9 +1,16 @@
 from email.policy import default
-from market import db
+from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
+# I need the is_authenticated, is_active, and is_anonymous methods from login_manager,
+# and i know a Class that already has that methods, so lets inherit from that class
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), nullable=False, primary_key=True)
     username = db.Column(db.String(length=60), nullable=False, unique=True)
     email = db.Column(db.String(length=60), nullable=False, unique=True)
@@ -20,6 +27,9 @@ class User(db.Model):
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
         print(f"Hasheando contraseña {plain_text_password} con el resultado {self.password_hash}")
+    
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password) #Returns true or false
 
 class Item(db.Model):
     id = db.Column(db.Integer(), nullable=False, primary_key=True)
