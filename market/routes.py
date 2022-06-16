35bfb3e72d8502bae1a617c3b51576ee64c70d3e
Ramiro Krapp <krappramiro@disroot.org>
2022-06-16 20:06:03 -0300

    added the automatic login and modal for more info

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import flash, render_template, redirect, url_for
 from market.models import User, Item
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route('/market/')
+@login_required  # This is from flask_login, its configured in __init__.py
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -27,6 +28,9 @@ def register_page():
                               password=form.password_1.data)  # i think this calls the @property password and the setter in models.py
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(
+            f"Account created succesfully, you are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}:  # If there are errors from the validations
         for err_msg in form.errors.values():
@@ -37,23 +41,27 @@ def register_page():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login_page():
-    form = LoginForm() #Consigo mi form de forms.py
+    form = LoginForm()  # Consigo mi form de forms.py
     if form.validate_on_submit():  # validate_on_submit validates that all the info is valid
         # Verify if the users exists
-        attempted_user = User.query.filter_by(username = form.username.data).first()
+        attempted_user = User.query.filter_by(
+            username=form.username.data).first()
         # Get the user that is equal to the username in the form
         # AND if the user exists, verify that the password is correct
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
-            login_user(attempted_user) #Se loggea al usuario usando el modulo flask_login
+            # Se loggea al usuario usando el modulo flask_login
+            login_user(attempted_user)
             flash(
                 f"Hello {attempted_user.username}! You are logged in!", category='success')
             return redirect(url_for('market_page'))
         else:
-            flash('Username and Password do not match. Please try again', category='danger')
+            flash('Username and Password do not match. Please try again',
+                  category='danger')
     return render_template('login.html', form=form)
+
 
 @app.route('/logout/')
 def logout_page():
-    logout_user() #Esto es parte de flask_login, funciona porque declare en __init__.py que se use flask_login
+    logout_user()  # Esto es parte de flask_login, funciona porque declare en __init__.py que se use flask_login
     flash("Successfully logged out!", category='info')
     return redirect(url_for('home_page'))
